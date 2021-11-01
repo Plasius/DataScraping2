36d471ca5bf078ac73439a6ea09afcee95fa5a6e
birosbeni <Beni>
2021-11-01 18:10:00 +0100

    Create LinkedInLogin.py

---
+++ BirosBenedek/LinkedInLogin.py
@@ -0,0 +1,35 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.linkedin.com/")
+
+# login gombra kattintás
+login = driver.find_element_by_class_name("nav__button-secondary")
+actions = ActionChains(driver)
+actions.click(login)
+actions.perform()
+
+time.sleep(2)
+
+# adatok beírása
+username = "bcebitclub@gmail.com"
+password = "AsD1AsD2?"
+username_input = driver.find_element_by_id("username")
+password_input = driver.find_element_by_id("password")
+username_input.clear()
+password_input.clear()
+username_input.send_keys(username)
+password_input.send_keys(password)
+
+time.sleep(2)
+
+# sing in gomb megnyomása
+sing_in_button = driver.find_element_by_class_name("btn__primary--large")
+actions.click(sing_in_button)
+actions.perform()
+
