68c1976ffd4b3ebea922bc3d4ee748f2731aea58
peterszabados <93289223+peterszabados@users.noreply.github.com>
2021-11-02 18:34:36 +0100

    Add files via upload

---
+++ SzabadosPeter/profile.py
@@ -0,0 +1,22 @@
+class Profile:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    # ? mi legyen a konstruktorban és mi nem
+    # experience:
+    exp_name = None
+    exp_company = None
+    exp_start_date = None
+    exp_end_date = None
+
+    # study:
+    study_name = None
+    study_institution = None
+    study_start_date = None
+    study_end_date = None
+
+    # certificate:
+    cert_name = None
+    cert_institution = None
+    cert_date = None
