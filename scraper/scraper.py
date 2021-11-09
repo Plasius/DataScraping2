8fedde14f6db34c0fa8b2d26f2197ea64585a3d6
birosbeni <Beni>
2021-11-09 19:11:32 +0100

    Változók átírása

---
+++ scraper/scraper.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+def login(username, password):
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+
+    driver.get("https://www.linkedin.com/")
+    actions = ActionChains(driver)
+
+    # login gombra kattintás
+    login = driver.find_element_by_class_name("nav__button-secondary")
+    actions.click(login)
+    actions.perform()
+
+    time.sleep(2)
+
+    # adatok beírása
+    username_input = driver.find_element_by_id("username")
+    password_input = driver.find_element_by_id("password")
+    username_input.clear()
+    password_input.clear()
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+
+    time.sleep(2)
+
+    # sing in gomb megnyomása
+    sing_in_button = driver.find_element_by_class_name("btn__primary--large")
+    actions.click(sing_in_button)
+    actions.perform()
+
+
+
+
+
