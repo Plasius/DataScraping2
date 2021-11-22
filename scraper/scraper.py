9107002d53e577af58272b11cba01ef1b9e54436
Plasius <Plasius@users.noreply.github.com>
2021-11-22 08:26:51 +0100

    1st edu

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome import service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
+from profile import Profile
+from selenium.webdriver.common.by import By
 # txt imports
 from os import listdir
 from os.path import isfile, join
@@ -13,15 +15,15 @@ def login(username, password, driver):
     actions = ActionChains(driver)
 
     # login gombra kattintás
-    login = driver.find_element_by_class_name("nav__button-secondary")
+    login = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
     actions.click(login)
     actions.perform()
 
     time.sleep(2)
 
     # adatok beírása
-    username_input = driver.find_element_by_id("username")
-    password_input = driver.find_element_by_id("password")
+    username_input = driver.find_element(By.ID, "username")
+    password_input = driver.find_element(By.ID, "password")
     username_input.clear()
     password_input.clear()
     username_input.send_keys(username)
@@ -30,13 +32,14 @@ def login(username, password, driver):
     time.sleep(2)
 
     # sing in gomb megnyomása
-    sing_in_button = driver.find_element_by_class_name("btn__primary--large")
+    sing_in_button = driver.find_element(By.CLASS_NAME, "btn__primary--large")
     actions.click(sing_in_button)
     actions.perform()
 
 # bejelentkezés
 driver = webdriver.Chrome(service=Service("chromedriver.exe"))
-login("BARMIMAST", "KERLEK", driver)
+driver.maximize_window()
+login("xisari4995@luxiu2.com", "azsxdcfv", driver)
 
 
 # lists mappának az elérési útja
@@ -55,6 +58,18 @@ for file in txt_files:
         driver.get(link[0])
         time.sleep(2)
 
+        # INFOK KIGYŰJTÉSE
+        try:
+            p = Profile()
+            p.name = driver.find_element(By.CLASS_NAME, "text-heading-xlarge").text
+            target_uni = driver.find_element(By.CLASS_NAME, "pv-entity__school-name").text
+            target_edu = driver.find_elements(By.CLASS_NAME, "pv-entity__comma-item")
+            target_level = target_edu[0].text
+            target_major = target_edu[1].text
+
+            print("{0:>35}".format(p.name) + "\t\t\t" + "{0:>10}".format(target_uni) + "\t\t\t" + "{0:>10}".format(target_major) + "\t\t\t" + "{0:>10}".format(target_level) + "\n")
+        except:
+            print("HIBA")
 
     open_file.close()
 
