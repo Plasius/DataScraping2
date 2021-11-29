3941e15ef364455b55530b0413a6a8ed627c92c9
Plasius <Plasius@users.noreply.github.com>
2021-11-30 00:22:39 +0100

    Preparing scraper.py for delivery

--- scraper/profile.py
+++ scraper/profile.py
@@ -1,28 +1,27 @@
+#az export csv formátum miatt kell lecserélni az adatokban lévő vesszőket
 class Profile:
-    def __init__(self):
-        self.name = ''
-        self.position = ''
+    def __init__(self, name, position, exp_name, exp_company, exp_start_date, exp_end_date, study_name, study_institution, study_start_date, study_end_date, cert_name, cert_institution, cert_issued, cert_expires):
+        self.name = name.replace(",", ";")
+        self.position = position.replace(",", ";")
 
         #experience
-        self.exp_name = ''
-        self.exp_company = ''
-        self.exp_start_date = ''
-        self.exp_end_date = ''
+        self.exp_name = exp_name.replace(",", ";")
+        self.exp_company = exp_company.replace(",", ";")
+        self.exp_start_date = exp_start_date.replace(",", ";")
+        self.exp_end_date = exp_end_date.replace(",", ";")
 
         # study:
-        self.study_name = ''
-        self.study_institution = ''
-        self.study_start_date = ''
-        self.study_end_date = ''
+        self.study_name = study_name.replace(",", ";")
+        self.study_institution = study_institution.replace(",", ";")
+        self.study_start_date = study_start_date.replace(",", ";")
+        self.study_end_date = study_end_date.replace(",", ";")
 
         # certificate:
-        self.cert_name = ''
-        self.cert_institution = ''
-        self.cert_date = ''
+        self.cert_name = cert_name.replace(",", ";")
+        self.cert_institution = cert_institution.replace(",", ";")
+        self.cert_issued = cert_issued.replace(",", ";")
+        self.cert_expires = cert_expires.replace(",", ";")
 
     # Ez a function hívodik meg amikor egy Profile object-et belerakunk egy print() parancsba
     def __str__(self):
-        print(self.name, self.position, self.exp_name)
-        print(self.exp_company, self.exp_start_date, self.exp_end_date)
-        print(self.study_name, self.study_institution, self.study_start_date, self.study_end_date)
-        print(self.cert_name, self.cert_institution, self.cert_date)
+        return f"{self.name},{self.position},{self.exp_name},{self.exp_company},{self.exp_start_date},{self.exp_end_date},{self.study_name},{self.study_institution},{self.study_start_date}, {self.study_end_date},{self.cert_name},{self.cert_institution},{self.cert_issued},{self.cert_expires}"
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,14 +1,45 @@
 # login imports
 import time
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome import service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
-from profile import Profile
-from selenium.webdriver.common.by import By
 # txt imports
-from os import listdir
+from os import listdir, name, replace
 from os.path import isfile, join
+import profile
+
+#ha változna a linkedin oldala, akkor csak itt kell átírni az xpatheket
+namePath = '//section/div[2]/div[2]/div[1]/div[1]/h1'
+positionPath1 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[2]/span[2]'
+positionPath2 = '//section/div[2]/div[2]/div[1]/div[2]'
+exp_namePath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/h3'
+exp_namePath2 = '//section/ul/li[1]/section/ul/li[1]/div/div/div/div/div/div/h3/span[2]' #amikor egy cégnél több állás is be van írva
+exp_companyPath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/p[2]'
+exp_companyPath2 = '//section/div[1]/section/ul/li[1]/section/div/a/div/div[2]/h3/span[2]'
+exp_datePath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/div/h4[1]/span[2]'
+exp_datePath2 = '/html/body/div[7]/div[3]/div/div/div/div/div[3]/div/div/main/div/div/div[5]/span/div/section/div[1]/section/ul/li[1]/section/ul/li[1]/div/div/div/div/div/div/div/h4[1]/span[2]'
+study_namePath1 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[2]/span[2]'
+study_namePath2 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p/span[2]'
+study_institutionPath = '//section/ul/li[1]/div/div/a/div[2]/div/h3'
+study_start_datePath = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/p/span[2]/time[1]'
+study_end_datePath = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/p/span[2]/time[2]'
+cert_namePath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/h3'
+cert_institutionPath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/p[1]/span[2]'
+cert_datePath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/p[2]/span[2]'
+
+
+
+def kinyeres(string, by):
+    if by.lower() == "class":
+        adat = driver.find_element(By.CLASS_NAME, string).text
+    elif by.lower() == "xpath":
+        adat = driver.find_element(By.XPATH, string).text
+    elif by.lower() == "id":
+        adat = driver.find_element(By.ID, string).text
+    
+    return adat
 
 def login(username, password, driver):
     driver.get("https://www.linkedin.com/")
@@ -22,8 +53,8 @@ def login(username, password, driver):
     time.sleep(2)
 
     # adatok beírása
-    username_input = driver.find_element(By.ID, "username")
-    password_input = driver.find_element(By.ID, "password")
+    username_input = driver.find_element(By.ID,"username")
+    password_input = driver.find_element(By.ID,"password")
     username_input.clear()
     password_input.clear()
     username_input.send_keys(username)
@@ -36,45 +67,130 @@ def login(username, password, driver):
     actions.click(sing_in_button)
     actions.perform()
 
+def export(profiles, fileName, header):
+    #"data.csv"
+    #"Név,Pozició,Tapasztalat,Cég,Kezdés,Vég,Szak,Egyetem,Kezdés,Vég,Oklevél,Kiállító,Dátum\n"
+
+    with open(fileName, "w", encoding="utf-8-sig") as f:
+        f.write(header)
+        for prof in profiles:
+            f.write(str(prof)+'\n')
+
 # bejelentkezés
 driver = webdriver.Chrome(service=Service("chromedriver.exe"))
 driver.maximize_window()
-login("xisari4995@luxiu2.com", "azsxdcfv", driver)
+login("bilebe2446@terasd.com", "azsxdcfv", driver)
 
 
 # lists mappának az elérési útja
-PATH = "lists"
+PATH = "liststest"
 
 # lista a fileokkal
 txt_files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
 
-# linkek beolvasása a fileokbol
+
+
 for file in txt_files:
     open_file = open(PATH + "\\" + file, 'r', encoding='utf-8')
+    linkedIn_links = [line.strip() for line in open_file]
 
-    linkedIn_links = [line.strip().split() for line in open_file]
+    profiles = list()
 
     for link in linkedIn_links:
-        driver.get(link[0])
-        time.sleep(2)
+        driver.get(link)
 
-        # INFOK KIGYŰJTÉSE
+        time.sleep(3)
         try:
-            p = Profile()
-            p.name = driver.find_element(By.CLASS_NAME, "text-heading-xlarge").text
-            target_uni = driver.find_element(By.CLASS_NAME, "pv-entity__school-name").text
-            target_edu = driver.find_elements(By.CLASS_NAME, "pv-entity__comma-item")
-            target_level = target_edu[0].text
-            target_major = target_edu[1].text
-
-            print("{0:>35}".format(p.name) + "\t\t\t" + "{0:>10}".format(target_uni) + "\t\t\t" + "{0:>10}".format(target_major) + "\t\t\t" + "{0:>10}".format(target_level) + "\n")
+            name = kinyeres(namePath, by="xpath")
         except:
-            print("HIBA")
+            print('Not a profile.')
+            continue
+        try:
+            position = kinyeres(positionPath1, by="xpath")
+        except:
+            try:
+                position = kinyeres(positionPath2, by="xpath")
+            except:
+                print("No position on this profile")
+                position = ""
+        try:
+            exp_name = kinyeres(exp_namePath1, by="xpath")
+        except:
+            try:
+                exp_name = kinyeres(exp_namePath2, by="xpath")
+            except:
+                print("No exp_name on this profile")
+                exp_name = ""
+        try:
+            exp_company = kinyeres(exp_companyPath1, by="xpath")
+        except:
+            try:
+                exp_company = kinyeres(exp_companyPath2, by="xpath")
+            except:
+                print("No exp_company on this profile")
+                exp_company = ""
+        exp_date = ""
+        try:
+            exp_date = kinyeres(exp_datePath1, by="xpath").split(" – ")
+        except:
+            try:
+                exp_date = kinyeres(exp_datePath2, by="xpath").split(" – ")
+            except:
+                print("No exp_date on this profile")
+                exp_start_date = ""
+                exp_end_date = ""
+        if exp_date != "":
+            exp_start_date = exp_date[0]
+            exp_end_date = exp_date[1]
+        try:
+            study_name = kinyeres(study_namePath1, by="xpath")
+        except:
+            try:
+                study_name = kinyeres(study_namePath2, by="xpath")
+            except:
+                print("No study_name on this profile")
+                study_name = ""
+        try:
+            study_institution = kinyeres(study_institutionPath, by="xpath")
+        except:
+            print("No study_institution on this profile")
+            study_institution = ""
+        try:
+            study_start_date = kinyeres(study_start_datePath, by="xpath")
+        except:
+            print("No study_start_date on this profile")
+            study_start_date = ""
+        try:
+            study_end_date = kinyeres(study_end_datePath, by="xpath")
+        except:
+            print("No study_end_date on this profile")
+            study_end_date = ""
+        try:
+            cert_name = kinyeres(cert_namePath, by="xpath")
+        except:
+            print("No cert_name on this profile")
+            cert_name = ""
+        try:
+            cert_institution = kinyeres(cert_institutionPath, by="xpath")
+        except:
+            print("No cert_institution on this profile")
+            cert_institution = ""
+        try:
+            cert_date = kinyeres(cert_datePath, by="xpath")
+            cert_issued = cert_date[7:15]
+            if cert_date[15] == "E":
+                cert_expires = cert_date[-8:]
+            else:
+                cert_expires = ""
+        except:
+            print("No cert_date on this profile")
+            cert_date = "" 
+            cert_issued = ""
+            cert_expires = ""           
+        
+        time.sleep(2)
+        profil = profile.Profile(name, position, exp_name, exp_company, exp_start_date, exp_end_date, study_name, study_institution, study_start_date, study_end_date, cert_name, cert_institution, cert_issued, cert_expires)
+        profiles.append(profil)
 
     open_file.close()
-
-
-
-
-
-
+    export(profiles, file[:-4]+'.csv', "name,position,exp_name,exp_company,exp_start_date,exp_end_date,study_name,study_institution,study_start_date,study_end_date,cert_name,cert_institution,cert_issued,cert_expires\n")
