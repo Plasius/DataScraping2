f7a3acee60498370f780fd8eff1baff8194fd9d1
Plasius <Plasius@users.noreply.github.com>
2021-12-10 00:56:08 +0100

    Polish

--- scraper/profile.py
+++ scraper/profile.py
@@ -1,6 +1,6 @@
 #az export csv formátum miatt kell lecserélni az adatokban lévő vesszőket
 class Profile:
-    def __init__(self, name, position, exp_name, exp_company, exp_start_date, exp_end_date, study_name, study_institution, study_start_date, study_end_date, cert_name, cert_institution, cert_issued, cert_expires):
+    def __init__(self, name, position, exp_name, exp_company, exp_start_date, exp_end_date,  study_institution, study_name, study_start_date, study_end_date):
         self.name = name.replace(",", ";")
         self.position = position.replace(",", ";")
 
@@ -16,12 +16,7 @@ class Profile:
         self.study_start_date = study_start_date.replace(",", ";")
         self.study_end_date = study_end_date.replace(",", ";")
 
-        # certificate:
-        self.cert_name = cert_name.replace(",", ";")
-        self.cert_institution = cert_institution.replace(",", ";")
-        self.cert_issued = cert_issued.replace(",", ";")
-        self.cert_expires = cert_expires.replace(",", ";")
 
     # Ez a function hívodik meg amikor egy Profile object-et belerakunk egy print() parancsba
     def __str__(self):
-        return f"{self.name},{self.position},{self.exp_name},{self.exp_company},{self.exp_start_date},{self.exp_end_date},{self.study_name},{self.study_institution},{self.study_start_date}, {self.study_end_date},{self.cert_name},{self.cert_institution},{self.cert_issued},{self.cert_expires}"
+        return f"{self.name},{self.position},{self.exp_name},{self.exp_company},{self.exp_start_date},{self.exp_end_date},{self.study_name},{self.study_institution},{self.study_start_date}, {self.study_end_date}"
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,33 +2,12 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.chrome import service
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 # txt imports
-from os import listdir, name, replace
+from os import listdir, name
 from os.path import isfile, join
 import profile
-
-#ha változna a linkedin oldala, akkor csak itt kell átírni az xpatheket
-namePath = '//section/div[2]/div[2]/div[1]/div[1]/h1'
-positionPath1 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[2]/span[2]'
-positionPath2 = '//section/div[2]/div[2]/div[1]/div[2]'
-exp_namePath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/h3'
-exp_namePath2 = '//section/ul/li[1]/section/ul/li[1]/div/div/div/div/div/div/h3/span[2]' #amikor egy cégnél több állás is be van írva
-exp_companyPath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/p[2]'
-exp_companyPath2 = '//section/div[1]/section/ul/li[1]/section/div/a/div/div[2]/h3/span[2]'
-exp_datePath1 = '//section/div[1]/section/ul/li[1]/section/div/div/a/div[2]/div/h4[1]/span[2]'
-exp_datePath2 = '/html/body/div[7]/div[3]/div/div/div/div/div[3]/div/div/main/div/div/div[5]/span/div/section/div[1]/section/ul/li[1]/section/ul/li[1]/div/div/div/div/div/div/div/h4[1]/span[2]'
-study_namePath1 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[2]/span[2]'
-study_namePath2 = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p/span[2]'
-study_institutionPath = '//section/ul/li[1]/div/div/a/div[2]/div/h3'
-study_start_datePath = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/p/span[2]/time[1]'
-study_end_datePath = '//section/div[2]/section/ul/li[1]/div/div/a/div[2]/p/span[2]/time[2]'
-cert_namePath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/h3'
-cert_institutionPath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/p[1]/span[2]'
-cert_datePath = '//section/div[3]/section/ul/li[1]/div/a/div[2]/p[2]/span[2]'
-
 
 
 def kinyeres(string, by):
@@ -38,35 +17,24 @@ def kinyeres(string, by):
         adat = driver.find_element(By.XPATH, string).text
     elif by.lower() == "id":
         adat = driver.find_element(By.ID, string).text
-    
     return adat
 
 def login(username, password, driver):
-    driver.get("https://www.linkedin.com/")
-    time.sleep(2)
-    actions = ActionChains(driver)
-
-    # login gombra kattintás
-    login = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
-    actions.click(login)
-    actions.perform()
-
+    driver.get("https://www.linkedin.com/login")
     time.sleep(2)
 
     # adatok beírása
     username_input = driver.find_element(By.ID,"username")
-    password_input = driver.find_element(By.ID,"password")
-    username_input.clear()
-    password_input.clear()
     username_input.send_keys(username)
-    password_input.send_keys(password)
-
     time.sleep(2)
 
-    # sing in gomb megnyomása
-    sing_in_button = driver.find_element(By.CLASS_NAME, "btn__primary--large")
-    actions.click(sing_in_button)
-    actions.perform()
+
+    password_input = driver.find_element(By.ID,"password")
+    password_input.send_keys(password)
+    time.sleep(2)
+
+    password_input.send_keys(Keys.RETURN)
+
 
 def export(profiles, fileName, header):
     with open('output'+'\\'+fileName, "w", encoding="utf-8-sig") as f:
@@ -92,106 +60,170 @@ txt_files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
 
 
 for file in txt_files:
-    open_file = open(PATH + "\\" + file, 'r', encoding='utf-8')
-    linkedIn_links = [line.strip() for line in open_file]
+    organizationFile = open(PATH + "\\" + file, 'r', encoding='utf-8')
+    urls = [line.strip() for line in organizationFile]
 
     profiles = list()
 
-    for link in linkedIn_links:
+    for link in urls:
         driver.get(link)
+        time.sleep(5)
 
-        time.sleep(2)
         try:
-            name = kinyeres(namePath, by="xpath")
+            name = kinyeres('text-heading-xlarge', by='class')
         except:
-            print('Not a profile.')
+            print('Not a profile:', link)
             continue
+
         try:
-            position = kinyeres(positionPath1, by="xpath")
+            bio = kinyeres('text-body-medium', by="class")
+        except:
+            print("No bio on this profile")
+            bio = ""
+
+        #EXPERIENCE
+        position = ''
+        company = ''
+        exp_start_date = ''
+        exp_end_date = ''
+
+
+        # EXP BLOCK 1
+        try:
+            experience_block = driver.find_element(By.CLASS_NAME, 'pv-entity__summary-info')
+            position = experience_block.find_element(By.CLASS_NAME, 't-16').text
+
+            uni_block = driver.find_element(By.CLASS_NAME, 'pv-entity__degree-info')
+            university = uni_block.find_element(By.CLASS_NAME, 'pv-entity__school-name').text
+
+            if position == university:
+                experience_block = ''
+                position = ''
         except:
             try:
-                position = kinyeres(positionPath2, by="xpath")
+                experience_block = driver.find_element(By.CLASS_NAME, 'pv-entity__summary-info')
             except:
-                print("No position on this profile")
-                position = ""
+                experience_block = ''
+
+        if experience_block != '':
+            try:
+                position = experience_block.find_element(By.CLASS_NAME, 't-16').text
+            except:
+                position = ''
+            
+            try:
+                company =  experience_block.find_element(By.CLASS_NAME, 'pv-entity__secondary-title').text
+                if company[-4:] == 'time':
+                    company = ' '.join(company.split(' ')[:-1])
+            except:
+                company = ''
+
+            try:
+                exp_date = experience_block.find_element(By.CLASS_NAME, 'pv-entity__date-range')
+                exp_date = exp_date.find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
+                if(len(exp_date)>4):
+                        exp_start_date = exp_date.split('–')[0]
+                        exp_end_date = exp_date.split('–')[1].split(' · ')[0]
+                else:
+                    exp_start_date = exp_date
+            except:
+                exp_start_date = ''
+                exp_end_date = ''
+        else:
+            pass
+
+        # EXP BLOCK 2
         try:
-            exp_name = kinyeres(exp_namePath1, by="xpath")
+            experience_block = driver.find_element(By.CLASS_NAME, 'pv-entity__summary-info-v2')
+            position = experience_block.find_element(By.CLASS_NAME, 't-14').find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
+
+            uni_block = driver.find_element(By.CLASS_NAME, 'pv-entity__degree-info')
+            university = uni_block.find_element(By.CLASS_NAME, 'pv-entity__school-name').text
+
+            if position == university:
+                experience_block = ''
+                position = ''
         except:
             try:
-                exp_name = kinyeres(exp_namePath2, by="xpath")
+                experience_block = driver.find_element(By.CLASS_NAME, 'pv-entity__summary-info-v2')
             except:
-                print("No exp_name on this profile")
-                exp_name = ""
-        try:
-            exp_company = kinyeres(exp_companyPath1, by="xpath")
-        except:
+                experience_block = ''
+
+
+        if experience_block != '':
             try:
-                exp_company = kinyeres(exp_companyPath2, by="xpath")
+                position = experience_block.find_element(By.CLASS_NAME, 't-14').find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
             except:
-                print("No exp_company on this profile")
-                exp_company = ""
-        exp_date = ""
-        try:
-            exp_date = kinyeres(exp_datePath1, by="xpath").split(" – ")
-        except:
+                position = ''
+            
             try:
-                exp_date = kinyeres(exp_datePath2, by="xpath").split(" – ")
+                company =  driver.find_elements(By.CLASS_NAME, 'pv-entity__company-summary-info')[0].find_elements(By.CLASS_NAME, 't-16')[0].find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
+                if company[-4:] == 'time':
+                    company = ' '.join(company.split(' ')[:-1])
             except:
-                print("No exp_date on this profile")
-                exp_start_date = ""
-                exp_end_date = ""
-        if exp_date != "":
-            exp_start_date = exp_date[0]
-            exp_end_date = exp_date[1]
-        try:
-            study_name = kinyeres(study_namePath1, by="xpath")
-        except:
+                company = ''
+            
             try:
-                study_name = kinyeres(study_namePath2, by="xpath")
+                exp_date = experience_block.find_element(By.CLASS_NAME, 'pv-entity__date-range')
+                exp_date = exp_date.find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
+                if(len(exp_date)>4):
+                        exp_start_date = exp_date.split('–')[0]
+                        exp_end_date = exp_date.split('–')[1].split(' · ')[0]
+                else:
+                    exp_start_date = exp_date
             except:
-                print("No study_name on this profile")
-                study_name = ""
-        try:
-            study_institution = kinyeres(study_institutionPath, by="xpath")
-        except:
-            print("No study_institution on this profile")
-            study_institution = ""
-        try:
-            study_start_date = kinyeres(study_start_datePath, by="xpath")
-        except:
-            print("No study_start_date on this profile")
-            study_start_date = ""
-        try:
-            study_end_date = kinyeres(study_end_datePath, by="xpath")
-        except:
-            print("No study_end_date on this profile")
-            study_end_date = ""
-        try:
-            cert_name = kinyeres(cert_namePath, by="xpath")
-        except:
-            print("No cert_name on this profile")
-            cert_name = ""
-        try:
-            cert_institution = kinyeres(cert_institutionPath, by="xpath")
-        except:
-            print("No cert_institution on this profile")
-            cert_institution = ""
-        try:
-            cert_date = kinyeres(cert_datePath, by="xpath")
-            cert_issued = cert_date[7:15]
-            if cert_date[15] == "E":
-                cert_expires = cert_date[-8:]
-            else:
-                cert_expires = ""
-        except:
-            print("No cert_date on this profile")
-            cert_date = "" 
-            cert_issued = ""
-            cert_expires = ""           
+                exp_start_date = ''
+                exp_end_date = ''
+        else:
+            pass
+            
+
         
-        time.sleep(2)
-        profil = profile.Profile(name, position, exp_name, exp_company, exp_start_date, exp_end_date, study_name, study_institution, study_start_date, study_end_date, cert_name, cert_institution, cert_issued, cert_expires)
+        # STUDY
+        
+        try:
+           uni_block = driver.find_element(By.CLASS_NAME, 'pv-entity__degree-info') 
+        except:
+            uni_block = ''
+            print('uni block not found', name)
+
+
+        if uni_block != '':
+            try:
+                university = uni_block.find_element(By.CLASS_NAME, 'pv-entity__school-name').text
+            except:
+                university = ''
+
+            try:
+                degree = uni_block.find_element(By.CLASS_NAME, 'pv-entity__fos').find_element(By.CSS_SELECTOR, 'span:nth-child(2)').text
+            except:
+                try:
+                    degree = uni_block.find_element(By.CLASS_NAME, 'pv-entity__comma-item').text
+                except:
+                    degree = ''
+
+            try:
+                uni_block = driver.find_elements(By.CLASS_NAME, 'pv-entity__dates')[0]
+                study_start_date = uni_block.find_element(By.CSS_SELECTOR, 'span:nth-child(2)').find_element(By.CSS_SELECTOR, 'time:nth-child(1)').text
+                study_end_date = uni_block.find_element(By.CSS_SELECTOR, 'span:nth-child(2)').find_element(By.CSS_SELECTOR, 'time:nth-child(2)').text
+            except:
+                try:
+                    study_start_date = uni_block.find_element(By.CSS_SELECTOR, 'span:nth-child(2)').find_element(By.CSS_SELECTOR, 'time:nth-child(1)').text
+                    study_end_date = ''
+                except:
+                    study_start_date = ''
+                    study_end_date = ''
+        else:
+            university = ''
+            degree = ''
+            study_start_date = ''
+            study_end_date = ''
+        
+
+        profil = profile.Profile(name, bio, position, company, exp_start_date, exp_end_date, university, degree, study_start_date, study_end_date)
         profiles.append(profil)
 
-    open_file.close()
-    export(profiles, file[:-4]+'.csv', 'Név,Pozició,Tapasztalat,Cég,Kezdés,Vég,Szak,Egyetem,Kezdés,Vég,Oklevél,Kiállító,Dátum\n')
+    organizationFile.close()
+    export(profiles, file[:-4]+'.csv', 'Név,Bio,Pozició,Cég,Kezdés,Vég,Szak,Egyetem,Kezdés,Vég\n')
+
+driver.close()
